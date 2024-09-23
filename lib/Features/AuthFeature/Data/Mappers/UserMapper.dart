32a4238902a489aba757d74c083d18c7ef6fe2c6
zeyadali06 <z01551153743@gmail.com>
2024-09-23 16:52:26 +0300

    create user mapper

---
+++ lib/Features/AuthFeature/Data/Mappers/UserMapper.dart
@@ -0,0 +1,13 @@
+import 'package:weather_app/Features/AuthFeature/Data/Models/UserModel.dart';
+import 'package:weather_app/Features/AuthFeature/Domain/Entities/LoginEntity.dart';
+import 'package:weather_app/Features/AuthFeature/Domain/Entities/RegisterEntity.dart';
+
+class UserMapper {
+  static LoginEntity toLoginEntity(UserModel userModel) {
+    return LoginEntity(email: userModel.email);
+  }
+
+  static RegisterEntity toRegisterEntity(UserModel userModel) {
+    return RegisterEntity(email: userModel.email, fullName: userModel.fullName);
+  }
+}
