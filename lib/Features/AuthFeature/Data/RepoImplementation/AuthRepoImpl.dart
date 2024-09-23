e972205409702d77eaef0bbd3f1de43093c8e2ef
zeyadali06 <z01551153743@gmail.com>
2024-09-23 15:41:09 +0300

    finish auth repo impl

---
+++ lib/Features/AuthFeature/Data/RepoImplementation/AuthRepoImpl.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:weather_app/Core/Failure/FirebaseFailure.dart';
+import 'package:weather_app/Core/Failure/FirebaseFailureHandler.dart';
+import 'package:weather_app/Features/AuthFeature/Data/Models/UserModel.dart';
+import 'package:weather_app/Features/AuthFeature/Domain/Entities/LoginEntity.dart';
+import 'package:weather_app/Features/AuthFeature/Data/DataSource/Registration.dart';
+import 'package:weather_app/Features/AuthFeature/Domain/RepoInterface/AuthRepo.dart';
+import 'package:weather_app/Features/AuthFeature/Domain/Entities/RegisterEntity.dart';
+import 'package:weather_app/Features/AuthFeature/Data/DataSource/FirebaseFirestoreServices.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  @override
+  Future<FirebaseResult> login(LoginEntity loginData, String password) async {
+    try {
+      UserCredential user = await SignIn.signIn(loginData.email!, password);
+      String fullName = await Firestore.getField(collectionPath: ConstantNames.usersDataCollection, docName: user.user!.uid, key: ConstantNames.fullName);
+      UserModel userModel = UserModel(email: loginData.email, uid: user.user!.uid, fullName: fullName);
+      return FirebaseResult.success(userModel);
+    } catch (e) {
+      return FirebaseResult.failure(FirebaseFailureHandler(e));
+    }
+  }
+
+  @override
+  Future<FirebaseResult> register(RegisterEntity registerData, String password) async {
+    try {
+      UserCredential user = await Register.register(registerData.toMap(), password);
+      UserModel userModel = UserModel(email: registerData.email, uid: user.user!.uid, fullName: registerData.fullName);
+      return FirebaseResult.success(userModel);
+    } catch (e) {
+      return FirebaseResult.failure(FirebaseFailureHandler(e));
+    }
+  }
+}
--- lib/Features/AuthFeature/Domain/RepoInterface/AuthRepo.dart
+++ lib/Features/AuthFeature/Domain/RepoInterface/AuthRepo.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:weather_app/Core/Failure/FirebaseFailure.dart';
 import 'package:weather_app/Features/AuthFeature/Domain/Entities/LoginEntity.dart';
 import 'package:weather_app/Features/AuthFeature/Domain/Entities/RegisterEntity.dart';
 
 abstract class AuthRepo {
-  Future<FirebaseResult<UserCredential>> register(RegisterEntity registerData, String password);
-  Future<FirebaseResult<UserCredential>> login(LoginEntity loginData, String password);
+  Future<FirebaseResult> register(RegisterEntity registerData, String password);
+  Future<FirebaseResult> login(LoginEntity loginData, String password);
 }
