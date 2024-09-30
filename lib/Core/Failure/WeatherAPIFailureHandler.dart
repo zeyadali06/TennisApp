278679ec0ab0468de9326aec85e63af5bfcafffc
zeyadali06 <z01551153743@gmail.com>
2024-09-30 21:18:17 +0300

    enhance weather api failure handler

--- lib/Core/Failure/WeatherAPIFailureHandler.dart
+++ lib/Core/Failure/WeatherAPIFailureHandler.dart
@@ -2,11 +2,13 @@ import 'package:tennis_app/Core/Failure/RequestFailure.dart';
 
 class WeatherAPIFailureHandler extends RequestFailed {
   late String message;
-  WeatherAPIFailureHandler(int errorCode) : super(null) {
-    if (errorCode == 1003) {
-      message = 'Search for anything';
-    } else if (errorCode == 1006) {
-      message = 'Please, enter valid location';
+  WeatherAPIFailureHandler(Object e) : super(null) {
+    if (e is int) {
+      if (e == 1003) {
+        message = 'Search for anything';
+      } else if (e == 1006) {
+        message = 'Please, enter valid location';
+      }
     } else {
       message = 'Error try again later';
     }
