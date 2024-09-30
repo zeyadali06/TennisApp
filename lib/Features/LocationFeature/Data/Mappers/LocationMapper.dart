0d06f5135014086d44ad3259d68d5fc26bc00cf2
zeyadali06 <z01551153743@gmail.com>
2024-10-01 00:55:10 +0300

    create place and position entities and mappers

---
+++ lib/Features/LocationFeature/Data/Mappers/LocationMapper.dart
@@ -0,0 +1,14 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:tennis_app/Features/LocationFeature/Data/Models/PlaceModel.dart';
+import 'package:tennis_app/Features/LocationFeature/Domain/Entities/PlaceEntity.dart';
+import 'package:tennis_app/Features/LocationFeature/Domain/Entities/PositionEntity.dart';
+
+class LocationMapper {
+  static PlaceEntity toPlaceEntity(PlaceModel placeModel) {
+    return PlaceEntity(longitude: placeModel.lon, latitude: placeModel.lat, name: placeModel.name, country: placeModel.country);
+  }
+
+  static PositionEntity toPositionEntity(Position position) {
+    return PositionEntity(longitude: position.longitude, latitude: position.latitude);
+  }
+}
---
+++ lib/Features/LocationFeature/Domain/Entities/PositionEntity.dart
@@ -0,0 +1,6 @@
+class PositionEntity {
+  final double longitude;
+  final double latitude;
+
+  const PositionEntity({required this.longitude, required this.latitude});
+}
