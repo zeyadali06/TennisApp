3cff543191cfb670b6c2191d51255f0d9f43f392
zeyadali06 <z01551153743@gmail.com>
2024-10-14 18:32:47 +0300

    create ai model services class

---
+++ lib/Features/HomeFeature/Data/DataSource/AIModelServices.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+
+class AIModelServices {
+  AIModelServices({required this.dio});
+
+  final Dio dio;
+
+  Future getPrediction(List<int> features) async {
+    var headers = {'Content-Type': 'application/json'};
+    var data = json.encode({"features": features});
+
+    var response = await dio.request(
+      '10.0.2.2:5001/predict',
+      options: Options(method: 'POST', headers: headers),
+      data: data,
+    );
+
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      return response.statusMessage;
+    }
+  }
+}
