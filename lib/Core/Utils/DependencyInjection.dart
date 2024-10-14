9bb54b90d9adbb31779dbe5622272da9bb14beef
zeyadali06 <z01551153743@gmail.com>
2024-10-14 22:13:10 +0300

    add ai model services to dependency injection

--- lib/Core/Utils/DependencyInjection.dart
+++ lib/Core/Utils/DependencyInjection.dart
@@ -1,11 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:tennis_app/Features/AuthFeature/Data/DataSource/Authentication.dart';
+import 'package:tennis_app/Features/HomeFeature/Data/DataSource/AIModelServices.dart';
 import 'package:tennis_app/Features/HomeFeature/Data/DataSource/WeatherApiServices.dart';
 import 'package:tennis_app/Features/HomeFeature/Data/RepoImplementation/HomeRepoImpl.dart';
+import 'package:tennis_app/Features/HomeFeature/Domain/UseCases/GetPredictionUseCase.dart';
 import 'package:tennis_app/Features/LocationFeature/Data/DataSource/LocationServices.dart';
 import 'package:tennis_app/Features/AuthFeature/Data/RepoImplementation/AuthRepoImpl.dart';
 import 'package:tennis_app/Features/AuthFeature/Domain/UseCases/ForgetPasswordUseCase.dart';
+import 'package:tennis_app/Features/HomeFeature/Data/RepoImplementation/AIModelRepoImpl.dart';
 import 'package:tennis_app/Features/AuthFeature/Data/DataSource/FirebaseFirestoreServices.dart';
 import 'package:tennis_app/Features/LocationFeature/Data/RepoImplementation/LocationRepoImpl.dart';
 import 'package:tennis_app/Features/LocationFeature/Data/RepoImplementation/LocationManagerRepoImpl.dart';
@@ -13,41 +16,44 @@ import 'package:tennis_app/Features/LocationFeature/Data/RepoImplementation/Loca
 GetIt getit = GetIt.instance;
 
 void setup() {
-  getit.registerSingleton<Dio>(
-    Dio(),
-  );
+  getit.registerSingleton<Dio>(Dio());
 
-  getit.registerSingleton<SignIn>(
-    SignIn(),
-  );
+  getit.registerSingleton<SignIn>(SignIn());
 
-  getit.registerSingleton<Register>(
-    Register(),
-  );
+  getit.registerSingleton<Register>(Register());
 
-  getit.registerSingleton<AccountData>(
-    AccountData(),
-  );
+  getit.registerSingleton<AccountData>(AccountData());
 
-  getit.registerSingleton<Firestore>(
-    Firestore(),
-  );
+  getit.registerSingleton<Firestore>(Firestore());
 
   getit.registerSingleton<WeatherApiServices>(
-    WeatherApiServices(
-      dio: getit.get<Dio>(),
-    ),
+    WeatherApiServices(dio: getit.get<Dio>()),
   );
 
   getit.registerSingleton<LocationServices>(
-    LocationServices(
-      dio: getit.get<Dio>(),
-    ),
+    LocationServices(dio: getit.get<Dio>()),
   );
 
   getit.registerSingleton<LocationManagerRepoImpl>(
-    LocationManagerRepoImpl(
-      firestore: getit.get<Firestore>(),
+    LocationManagerRepoImpl(firestore: getit.get<Firestore>()),
+  );
+
+  getit.registerSingleton<LocationRepoImpl>(
+    LocationRepoImpl(locationServices: getit.get<LocationServices>()),
+  );
+
+  getit.registerSingleton<AIModelServices>(
+    AIModelServices(dio: getit.get<Dio>()),
+  );
+
+  getit.registerSingleton<AIModelRepoImpl>(
+    AIModelRepoImpl(aiModelServices: getit.get<AIModelServices>()),
+  );
+
+  getit.registerSingleton<GetPredictionUseCase>(
+    GetPredictionUseCase(
+      aiModelRepo: getit.get<AIModelRepoImpl>(),
+      homeRepo: getit.get<HomeRepoImpl>(),
     ),
   );
 
@@ -55,12 +61,6 @@ void setup() {
     HomeRepoImpl(
       weatherApiServices: getit.get<WeatherApiServices>(),
       locationManagerRepo: getit.get<LocationManagerRepoImpl>(),
-    ),
-  );
-
-  getit.registerSingleton<LocationRepoImpl>(
-    LocationRepoImpl(
-      locationServices: getit.get<LocationServices>(),
     ),
   );
 
